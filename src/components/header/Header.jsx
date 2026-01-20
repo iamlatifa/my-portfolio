98065f31aa46384bf43a14132ded58fa7e141855
latifa2003 <anayousslele@gmail.com>
2026-01-20 21:31:42 +0100

    fix bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./header.css";
 import HeaderIntro from "./HeaderIntro";
 import HeaderAboutCard from "./HeaderAboutCard";
+import bg from "../../assets/background-home.png";
 
 const Header = () => {
   return (
-    <header id="header">
+
+    <header style={{ "--bg-img": `url(${bg})` }}>
       <div className="container header_container">
         <HeaderIntro />
         <HeaderAboutCard />
